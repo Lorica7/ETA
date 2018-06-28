bacd07a16648eade1c25c6d92e128f6881307ef6
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-06-27 20:07:03 -0400

    minor changes HTML, wrote firebase related code

--- logic.js
+++ logic.js
@@ -1,34 +1,58 @@
 
-  // Initialize Firebase
+// Initialize Firebase
 
-  var config = {
+var config = {
     apiKey: "AIzaSyCqtEwFWwVVdd5teCAI_htzh3MaxS2oxSw",
     authDomain: "trainscheduleapp-da14d.firebaseapp.com",
     databaseURL: "https://trainscheduleapp-da14d.firebaseio.com",
     projectId: "trainscheduleapp-da14d",
     storageBucket: "trainscheduleapp-da14d.appspot.com",
     messagingSenderId: "752893953291"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
+//Create variables
+
+var train = "";
+var destination = "";
+var firstTrain = "";
+var frequency = 0;
+
+$("#submitButton").on('click', function () {
+    console.log(hello);
+    train = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#firstTrain").val().trim();
+    frequency = $("#frequency").val().trim();
+
+firebase.database().ref().set({
+    train: train,
+    destination: destination,
+    firstTrain : firstTrain,
+    frequency: frequency,
+});
+});
 
 
-//Example of a listener in Firebase
+
+
+//Example of a listener in Firebase and code for counters
 var count = 100;
 
-firebase.database().ref().on('value', function(snapshot){
+firebase.database().ref().on('value', function (snapshot) {
     snapshot.log(snapshot.val());
     count = snapshot.val().clicks;
     $("#click-value").html(count);
 });
 
-$("#clickButton").on('click', function(){
+$("#clickButton").on('click', function () {
     count--;
     firebase.database().ref().set({
         clicks: count
     });
 
 });
-$("#restartButton").on('click', function(){
+$("#restartButton").on('click', function () {
     firebase.database.ref().set({
         clicks: 100,
     });
